787db44d4a16c8ac108801797645f8ffcba9df2c
Angelo Berios <rubyblackbelt@angelo.ca>
2018-06-21 10:32:59 -0400

    Make user accessible on client-side

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def acting_user
+    current_user
+  end
 end
--- app/hyperloop/models/user.rb
+++ app/hyperloop/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable unless RUBY_ENGINE == 'opal'
+
+  def self.current
+    Hyperloop::Application.acting_user_id ? find(Hyperloop::Application.acting_user_id) : User.new
+  end
+
+  class << self
+    alias _new_without_sti_type_cast new
+  end
 end
